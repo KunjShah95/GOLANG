5b4680aef31d8619d78262f43530c90988461b23
KunjShah95 <kkshah05072005@gmail.com>
2024-12-10 07:01:35 +0530

    first commit

---
+++ GO/chapter3/main.go
@@ -0,0 +1,58 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var x string
+// 	x = "Hello World"
+// 	fmt.Println(x)
+// }
+
+//another method
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var x string
+// 	x = "first"
+// 	fmt.Println(x)
+// 	x = "second"
+// 	fmt.Println(x)
+// }
+
+//another method
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var x string
+// 	x = "first"
+// 	fmt.Println(x)
+// 	x = x + "second"
+// 	fmt.Println(x)
+// }
+
+// another method
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	var x string = "hello"
+// 	var y string = "world"
+// 	fmt.Println(x == y)
+// }
+//another method
+package main
+
+import "fmt"
+
+func main() {
+	var x string = "hello"
+	var y string = "hello"
+	fmt.Println(x == y)
+}
---
+++ GO/chapter3/variable.go
@@ -0,0 +1,27 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	x := "Max"
+// 	fmt.Println("My name is", x)
+// }
+// another method
+
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	name := "Max"
+// 	fmt.Println("My name is", name)
+// }
+//another method
+package main
+
+import "fmt"
+
+func main() {
+	myName := "Max"
+	fmt.Println("My name is", myName)
+}
